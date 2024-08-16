7ca57b3a6dbc0d729b152a097b1a06d55c8c80ef
Yeshwanth635 <yeshubg@gmail.com>
2024-08-16 22:00:34 +0530

    1st commit

---
+++ build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Defining the image name
+IMAGE_NAME="yeshwanth2435/dev"
+
+# Building the Docker image using Docker Compose
+echo "Building Docker image: $IMAGE_NAME"
+docker-compose build
+
+# Checking if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image built successfully: $IMAGE_NAME"
+else
+    echo "Error: Docker image build failed."
+    exit 1
+fi
