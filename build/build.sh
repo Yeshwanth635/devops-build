2398a4a55eae7141b80bd832c2aace45e814cad5
Yeshwanth635 <yeshubg@gmail.com>
2024-08-18 02:14:20 +0530

    updated

--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Defining the image name
-IMAGE_NAME="yeshwanth2435/dev"
-
+IMAGE_NAME=$1
+TAG="latest" 
 # Building the Docker image using Docker Compose..
 echo "Building Docker image: $IMAGE_NAME"
-docker compose build
+docker build -t $IMAGE_NAME:$TAG
 
 # Checking if the build was successful
 if [ $? -eq 0 ]; then
-    echo "Docker image built successfully: $IMAGE_NAME"
+    echo "Docker image built successfully: $IMAGE_NAME:$TAG"
 else
     echo "Error: Docker image build failed."
     exit 1
--- build/deploy.sh
+++ build/deploy.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
 # Define the image name and tag
-IMAGE_NAME="yeshwanth2435/dev"
+IMAGE_NAME=$1
 TAG="latest" 
-
-# Logging into Docker Hub
-echo "Logging into Docker Hub..."
-docker login
-
-# Checking if login was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Docker login failed."
-    exit 1
-else
-    echo "Docker login succesfull."
-fi
 
 # Pushing the Docker image to Docker Hub
 echo "Pushing Docker image: $IMAGE_NAME:$TAG to Docker Hub..."
