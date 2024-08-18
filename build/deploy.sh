d1b77123c979d04d8010d6bad257cfe7e08a7569
Yeshwanth635 <yeshubg@gmail.com>
2024-08-18 13:23:42 +0530

    updated

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,15 +2,14 @@
 
 # Define the image name and tag
 IMAGE_NAME=$1
-TAG="latest" 
 
 # Pushing the Docker image to Docker Hub
-echo "Pushing Docker image: $IMAGE_NAME:$TAG to Docker Hub..."
-docker push $IMAGE_NAME:$TAG
+echo "Pushing Docker image: $IMAGE_NAME to Docker Hub..."
+docker push $IMAGE_NAME
 
 # Check if the push was successful
 if [ $? -eq 0 ]; then
-    echo "Docker image pushed successfully: $IMAGE_NAME:$TAG"
+    echo "Docker image pushed successfully: $IMAGE_NAME"
 else
     echo "Error: Docker image push failed."
     exit 1
